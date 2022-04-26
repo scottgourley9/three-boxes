1b3e0b5f9e4f672b8abfbf6fe278c12361a6d58a
Scott <admin@gearproject.co>
2022-04-26 10:17:35 -0600

    comment out

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,11 @@ export default function Home() {
       <main>
         <h1 className={styles.title}>
           Three Boxes
-          <a class="ar active" href="intent://arvr.google.com/scene-viewer/1.0?mode=ar_preferred&amp;resizable=true&amp;file=https://scottgourley.s3.us-west-2.amazonaws.com/Green+Box.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;end;" rel="ar">
-            AR Testing
-          </a>
+          {/*
+              <a class="ar active" href="intent://arvr.google.com/scene-viewer/1.0?mode=ar_preferred&amp;resizable=true&amp;file=https://scottgourley.s3.us-west-2.amazonaws.com/Green+Box.glb#Intent;scheme=https;package=com.google.android.googlequicksearchbox;action=android.intent.action.VIEW;end;" rel="ar">
+                AR Testing
+              </a>
+          */}
         </h1>
         <div className={styles.box}>
             Box 1
